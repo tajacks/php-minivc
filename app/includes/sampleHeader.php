2fba6989c1681e204b71c30ebc3f34e6ec01f96a
tajacks <tom@tomjack.ca>
2022-01-14 16:17:09 -0500

    - Dynamic Title in header

--- app/includes/sampleHeader.php
+++ app/includes/sampleHeader.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Sample Title</title>
+    <title><?php echo($data['title']);?></title>
     <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
 </head>
 <body>
